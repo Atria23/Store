b75711592cc3c00394b7e2864184f108955355a2
Atria23 <danu03anggoro@gmail.com>
2024-10-20 17:17:09 +0700

    build again

---
+++ public/build/assets/InputLabel-WOs3cGej.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-K4T80rk6.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
