ca6351e83836f41a798faa7fcaceddf26367392f
Vausa24434 <muvausastore1@gmail.com>
2025-01-20 16:45:47 +0700

    add account setting page, add profile page, modif footer, modif popup edit store

--- public/build/assets/InputLabel-NnwMmk6t.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-qWG3QkvC.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
