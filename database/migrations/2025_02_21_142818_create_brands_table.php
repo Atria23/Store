948c2579a3fb6ce7ddde7f11aa08976f65b68d5a
Vausa24434 <muvausastore1@gmail.com>
2025-02-22 22:07:22 +0700

    add manage brand, modif price list and category controller

--- database/migrations/2025_02_21_142818_create_brands_table.php
+++ database/migrations/2025_02_21_142818_create_brands_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
             $table->string('image')->nullable();
             $table->string('name');
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
-            $table->foreignId('input_type_id')->nullable()->constrained('input_types')->onDelete('set null');
+            $table->foreignId('input_type_id')->nullable()->default(1)->constrained('input_types')->onDelete('set null');
             $table->decimal('profit_persen', 8, 2)->nullable()->default(0);
             $table->decimal('profit_tetap', 10, 2)->nullable()->default(0);
             $table->timestamps();
