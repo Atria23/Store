dde80470a56b8154f85fad68655b03d6dbf5d484
Vausa24434 <muvausastore1@gmail.com>
2025-05-17 10:13:29 +0700

    modif total pay history detail

--- resources/js/Pages/User/HistoryDetail.jsx
+++ resources/js/Pages/User/HistoryDetail.jsx
@@ -484,10 +484,12 @@ const HistoryDetail = () => {
                             <div className="w-full flex flex-row justify-between items-center">
                                 <span className="text-md font-utama font-medium text-gray-700">Total Bayar</span>
                                 <span className="text-md font-utama font-semibold text-black">
-                                    {formatRupiah(
+                                    {/* {formatRupiah(
                                         (isEditing ? Number(price) : Number(String(transaction.price).replace(/\D/g, ""))) +
                                         (isEditing ? Number(adminFee) : Number(String(transaction.admin_fee).replace(/\D/g, "")))
-                                    )}
+                                    )} */}
+                                    {formatRupiah(Number(price) + Number(adminFee))}
+
                                 </span>
                             </div>
                         </div>
