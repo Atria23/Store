296f39d9a5266770a105accc229c35712190b7d6
Vausa24434 <muvausastore1@gmail.com>
2025-05-18 07:17:03 +0700

    fix manage brand and types

--- resources/js/Pages/ManageTypes.jsx
+++ resources/js/Pages/ManageTypes.jsx
@@ -702,9 +702,10 @@ export default function ManageTypes({ types = [], brands = [], categories = [], 
 
                                                             {/* Jika tidak ada hasil */}
                                                             {brands.filter(bra =>
-                                                                (!data.category_id || bra.category_id === data.category_id) &&
+                                                                (!data.category_id || String(bra.category_id) === String(data.category_id)) &&
                                                                 bra.name.toLowerCase().includes(searchBrand.toLowerCase())
-                                                            ).length === 0 && (
+                                                            )
+                                                                .length === 0 && (
                                                                     <p className="text-gray-500 text-sm text-center w-full py-2">Brand tidak ditemukan</p>
                                                                 )}
                                                         </div>
